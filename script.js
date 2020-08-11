459e700819050a3fabd90a353a72344e3df4e297
Matt Volonnino <mvolonnino12@gmail.com>
2020-08-10 20:09:44 -0400

    got the ajax call working on city search, grabbed the lat & lon , weather description, temp, humidity, and wind speed - will have to use another ajax call to get UV index with lat & lon pulled from first ajax

--- script.js
+++ script.js
@@ -6,18 +6,38 @@ $(document).ready(function () {
     console.log("This is the city searched: ", city);
     var fahrenheitUnits = "&units=imperial";
     var apiKey = "&appid=" + "834f6c40f1d6279c2ecf1aa03eab085f";
-    var queryURL =
-      "api.openweathermap.org/data/2.5/weather?q=" +
+    var cityQueryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
       fahrenheitUnits +
       apiKey;
-    console.log("This will be the queryURL: ", queryURL);
+    console.log("This will be the queryURL: ", cityQueryURL);
 
     $.ajax({
-      url: queryURL,
+      url: cityQueryURL,
       method: "GET",
     }).then(function (response) {
+      //should be able to pull weather conditions, temperature in F, humidity, and windspeed - will need another ajax call that uses lon and lat to search for UV index
       console.log("this is our response: ", response);
+      var lon = response.coord.lon;
+      var lat = response.coord.lat;
+      console.log("This is longitude: ", lon, "this is latitude: ", lat);
+      var conditions = response.weather[0].description;
+      var temp = response.main.temp;
+      var humidity = response.main.humidity + "%";
+      var windSpeed = response.wind.speed + " mph";
+      console.log(
+        "weather conditions: ",
+        conditions,
+        "temperature: ",
+        temp,
+        "humidity: ",
+        humidity,
+        "wind speed: ",
+        windSpeed
+      );
     });
+
+    
   });
 });
